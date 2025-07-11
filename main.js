ebe29920fa4ead9ff42bbef7ebbb1aabb4c4c63b
Gen2micky1 <olowofelamicheal1@gmail.com>
2025-07-11 15:45:44 +0100

    Desktop view update

--- main.js
+++ main.js
@@ -1,32 +1,40 @@
-const shareButton = document.querySelector('.article-preview__share-icon');
-shareButton.addEventListener("click", () => {
-  const isPresent = shareButton.classList.toggle('active');
+const shareButton = document.querySelector(".article-preview__share-icon");
 
-  const wrapper = document.querySelector('.article-preview__share-socials-wrapper');
+shareButton.addEventListener("click", () => {
+  const isPresent = shareButton.classList.toggle("active");  // This will give us a boolean value
+  const wrapper = document.querySelector(
+    ".article-preview__share-socials-wrapper"
+  );
 
   if (!shareButton) return;
 
   if (isPresent) {
-    shareButton.style.backgroundColor = '#6E8098';
-    wrapper.style.display = 'block';
-    shareButton.innerHTML = `<img
-    src="./images/icon-share-alt.svg"
-    alt="icon-share-alt"
-    width="15"
-    height="13"
+    shareButton.style.backgroundColor = "#6e8098";
+    wrapper.style.display = "block";
+    shareButton.innerHTML = `
+    <img
+        src="./images/icon-share-alt.svg"
+        alt="icon-share"
+        width="15"
+        height="13"
     />
 
-    <figcaption class="offscreen">icon-share</figcaption>`;
+    <figcaption class="offscreen">icon-share
+    </figcaption>
+    `;
   } else {
-    shareButton.style.backgroundColor = '#ECF2F8';
-    wrapper.style.display = 'none';
-    shareButton.innerHTML = `<img
-    src="./images/icon-share.svg"
-    alt="icon-share"
-    width="15"
-    height="13"
+    shareButton.style.backgroundColor = "#ecf2f8";
+    wrapper.style.display = "none";
+    shareButton.innerHTML = `
+    <img
+        src="./images/icon-share.svg"
+        alt="icon-share"
+        width="15"
+        height="13"
     />
 
-    <figcaption class="offscreen">icon-share</figcaption>`;
+    <figcaption class="offscreen">icon-share
+    </figcaption>
+    `;
   }
 });
